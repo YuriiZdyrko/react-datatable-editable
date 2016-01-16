e55c57b2e47c7d5d018d610774da9a9038e5ee68
banzay <yuriy.zdyrko@gmail.com>
2016-01-16 11:42:43 +0200

    boilerplate

---
+++ app/main.js
@@ -0,0 +1,7 @@
+var Datatable = require('./datatable.js'),
+  ReactDOM = require('react-dom'),
+  React = require('react');
+
+var root = document.getElementById('root');
+
+ReactDOM.render(<Datatable />, root);
